662d7d9927608da171d45000d5ac4bd65f18b54c
wangpengyao <1810321410@qq.com>
2023-05-11 19:53:30 +0800

    array数组

---
+++ javaBase/day-01/src/array.java
@@ -0,0 +1,22 @@
+public class array {
+    public static void main(String[] args){
+//        初始化静态数组
+        int[] array = new int[]{1,3,4};
+        System.out.println(array);
+
+//        初始化动态数组
+        int[] array2 = new int[30];
+        for (int i = 0; i < array2.length; i++) {
+            System.out.println(array2[i]);
+        }
+
+
+        int[] zhandou = new int[]{33,5,22,44,55};
+        for (int i = 0; i < zhandou.length; i++) {
+            if(zhandou[0] < zhandou[i]) zhandou[0] = zhandou[i+1];
+
+        }
+        System.out.println("最大战力是:"+zhandou[0]);
+    }
+
+}
